56faea61e5142ef62efdf7d21b1baf314d64f470
Sanjay <sanjaylavel@gmail.com>
2026-06-01 21:02:15 +0530

    - Implement basic die roll animation - Test out web build

--- dice/animated_die.gd
+++ dice/animated_die.gd
@@ -3,8 +3,15 @@ extends Die
 class_name AnimatedDie
 
 @onready var path_follow_3d: PathFollow3D = $Path3D/PathFollow3D
+@onready var mesh_instance_3d: MeshInstance3D = $Path3D/PathFollow3D/Node3D/MeshInstance3D
 
 func _ready() -> void:
-	var tween = create_tween().set_parallel(true)
+	roll(randf())
 
-	tween.tween_property(path_follow_3d, "progress_ratio", 1.0, 0.5).set_trans(Tween.TRANS_QUAD).set_ease(Tween.EASE_OUT)
+func roll(p_distribution: float) -> int:
+	var ret = super(p_distribution)
+	var tween = create_tween().set_parallel(true)
+	mesh_instance_3d.rotate_object_local(Vector3.UP, deg_to_rad(randf_range(-10, 10)))
+
+	tween.tween_property(path_follow_3d, "progress_ratio", 1.0, 1.0 + randf() * 0.5).set_trans(Tween.TRANS_QUAD).set_ease(Tween.EASE_OUT)
+	return ret
